4cd5a9a4c8cdf674fa52154a214a09aec3aebe80
richlee33 <attnrichlee@gmail.com>
2024-08-20 10:39:00 -0700

    modify test values

--- test_success_rate.py
+++ test_success_rate.py
@@ -16,5 +16,5 @@ success_rate_std = df_summary.at['std', 2]
 assert success_rate_mean > .66, f"success rate mean is too small, got {success_rate_mean}"
 assert success_rate_mean < .70, f"success rate mean is too large, got {success_rate_mean}"
 
-assert success_rate_std > .020, f"success rate std is too small, got {success_rate_std}"
-assert success_rate_std < .024, f"success rate std is too large, got {success_rate_std}"
+assert success_rate_std > .01, f"success rate std is too small, got {success_rate_std}"
+assert success_rate_std < .03, f"success rate std is too large, got {success_rate_std}"
